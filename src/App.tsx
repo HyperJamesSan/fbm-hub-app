992bf4f47d973521e581dc7c8ea0626b0d620479
HyperJamesSan <jamesyesidsp@gmail.com>
2026-04-20 10:53:54 +0200

    Add basename to BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Hub />} />
           <Route path="/session-1" element={<Session1 />} />
